0bb194aa0ea4c5145190d6a92fda615c36025a0e
Tristan Guiraudou <trg9638@gmail.com>
2026-09-06 12:07:18 +0200

    Musée

--- components/date-invitation.tsx
+++ components/date-invitation.tsx
@@ -20,9 +20,9 @@ const ACTIVITIES: Activity[] = [
     icon: <Film className="size-7" aria-hidden="true" />,
   },
   {
-    id: "Baise",
-    label: "Une Baise",
-    description: "Direction le 7eme ciel !",
+    id: "Musée",
+    label: "Musée",
+    description: "Découvrez l'art et l'histoire",
     icon: <Trees className="size-7" aria-hidden="true" />,
   },
   {
